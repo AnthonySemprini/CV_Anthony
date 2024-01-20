5f774c867b3801addb34f827990a32ea6e185976
Anthony <semprini.anthony@gmail.com>
2024-01-20 01:04:10 +0100

    mise en page et fonctionalité de la home page

--- src/Portfolio/App.jsx
+++ src/Portfolio/App.jsx
@@ -6,13 +6,14 @@ import Stack from './Tools&Language/Tools&Language';
 
 function App() {
   return (
-    <div className=" general flex h-full">
-    <div className='  w-1/4 h-[100vh] flex justify-center bg-blueM m-3 p-5 rounded-lg'><Header /></div>
-    <div className=' w-3/4 h-[100vh] bg-amande  m-3 p-5 rounded-lg'><Profil /></div>
+  
+  <div className=" general flex h-full bg-orangeRouge">
+    <div className='  w-1/4 h-[99vh] flex justify-center bg-blueM m-1 p-5 rounded-lg'><Header /></div>
+    <div className='flex  w-3/4 h-[99vh] bg-amande  ml-0 mt-1 mr-1 mb-1 p-5 rounded-lg'><Profil /></div>
     {/* <div className='row-span-1 col-span-1  '><Stack /></div> */}
     {/* <div className='row-span-1 col-span-1'><MyButton /></div> */}
   </div>
 );
-}""
+}
 
-export default App
+export default App;
--- src/Portfolio/Home/Home.jsx
+++ src/Portfolio/Home/Home.jsx
@@ -21,12 +21,12 @@ const user = {
 function Profil() {
   return (
     <>
-      <div className=''>
-        <h1 className='text-3xl text-center p-5'>Bienvenue sur mon portfolio</h1>
-        <h2 className='text-center text-2xl p-5'>{user.name}</h2>
-        <p className='mx-5'>{user.description}</p>
+      <div className='flex flex-col justify-center'>
+        <h1 className='text-center text-blueM text-5xl p-5'>{user.name}</h1>
+        <h2 className='text-4xl text-blueM  p-5'>Dévellopeur Web</h2>
+        <p className='mx-5 text-xl text-blueM text-justify'>{user.description}</p>
+      <button className='mx-20 mt-20 bg-mangue border-4 border-orangeRouge  p-3 rounded-xl text-blueM font-bold'> <MyButton /></button>
       </div>
-      <h3 className='mt-5 flex justify-center '> <MyButton /></h3>
     </>
   );
 }
