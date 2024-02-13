2ceba625623eeb13890c679f81ea24187404ec8f
Anthony <semprini.anthony@gmail.com>
2024-02-13 19:11:37 +0100

    crea des carte stack

--- src/Portfolio/App.jsx
+++ src/Portfolio/App.jsx
@@ -9,9 +9,9 @@ function App() {
   
   <div className=" general flex h-full bg-orangeRouge">
     <div className='  w-1/4 h-[99vh] flex justify-center bg-blueM m-1 p-5 rounded-lg'><Header /></div>
-    <div className='flex  w-3/4 h-[99vh] bg-amande  ml-0 mt-1 mr-1 mb-1 p-5 rounded-lg'><Profil /></div>
-    {/* <div className='row-span-1 col-span-1  '><Stack /></div> */}
-    {/* <div className='row-span-1 col-span-1'><MyButton /></div> */}
+    {/* <div className='flex  w-3/4 h-[99vh] bg-amande  ml-0 mt-1 mr-1 mb-1 p-5 rounded-lg'><Profil /></div> */}
+    <div className='flex  w-3/4 h-[99vh] bg-amande  ml-0 mt-1 mr-1 mb-1 p-5 rounded-lg  '><Stack /></div>
+    
   </div>
 );
 }
--- src/Portfolio/Header/Header.jsx
+++ src/Portfolio/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import MyButton from '../CvButton/CvButton';
+
 
 function Header() {
   return (
