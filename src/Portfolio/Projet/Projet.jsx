220b153571890b9906d968a9a532b21da22b66e2
Anthony <semprini.anthony@gmail.com>
2024-02-22 18:37:10 +0100

    ajout d'une new carte projet et reglage front

--- src/Portfolio/Projet/Projet.jsx
+++ src/Portfolio/Projet/Projet.jsx
@@ -8,24 +8,27 @@ const projects = [
     id: 1,
     name: 'BarberShop',
     imageUrl: '/img/barber.png',
+    skills: 'https://skillicons.dev/icons?i=git,php,symfony,js,css,mysql',
     projectUrl: '',
     githubUrl: 'https://github.com/AnthonySemprini/barber',
-    description: 'Bienvenue dans mon projet de création d\'un site de réservation en ligne et click & collect pour coiffeurs-barbiers, visant à simplifier la gestion des rendez-vous et la vente de produits avec une interface intuitive. Ce site permet aux clients de réserver facilement leur créneau et d\'acheter des produits en click & collect, tandis que les professionnels bénéficient d\'outils de gestion des rendez-vous et des produits. Le site est développé avec PHP (Symfony), JavaScript, HTML, CSS, et utilise une base de données MySQL.'
+    description: 'Bienvenue dans mon projet de création d\'un site de réservation en ligne et click & collect pour coiffeurs-barbiers, simplifiant la gestion des rendez-vous et la vente de produits.'
   },
-  
+
   {
     id: 2,
     name: 'Blog Laravel',
     imageUrl: '/img/blog.png',
+    skills: 'https://skillicons.dev/icons?i=git,php,laravel,js,vue,tailwind',
     projectUrl: '',
     githubUrl: 'https://github.com/AnthonySemprini/MyFirstLaravelProjectBlog',
-    description: 'Le jeu de Morpion se joue sur une grille de 3x3 cases. Deux joueurs s\'affrontent et marquent à tour de rôle les cases de la grille avec leur symbole respectif, "X" pour le Joueur 1 et "O" pour le Joueur 2. Le gagnant est le premier joueur à aligner trois de ses symboles horizontalement, verticalement ou diagonalement. Si la grille est complètement remplie sans qu\'aucun joueur n\'ait aligné trois symboles, la partie est déclarée nulle.'
+    description: 'MyFirstLaravelProjectBlog est un système de gestion de blog qui permet la publication, la modification et la suppression d\'articles par un admin, offre la lecture publique et le \'like\' par les utilisateurs authentifiés, construit avec Laravel, Tailwind CSS, et Vue 3.'
   },
 
   {
     id: 3,
     name: 'Landing Page Kucra',
     imageUrl: '/img/kucra.png',
+    skills: 'https://skillicons.dev/icons?i=git,html,css,vscode',
     projectUrl: 'https://landing-page-kucra.netlify.app/',
     githubUrl: 'https://github.com/AnthonySemprini/exo-kucra',
     description: 'Kucra est une landing page élégante et moderne conçue en cour de ma formation chez elan formation en suivant une maquette. Ce modèle responsive s\'adapte parfaitement à tous les appareils grâce à ses feuilles de style CSS spécifiques.'
@@ -35,15 +38,17 @@ const projects = [
     id: 4,
     name: 'Session App',
     imageUrl: '/img/session.png',
+    skills: 'https://skillicons.dev/icons?i=git,php,symfony,bootstrap,mysql',
     projectUrl: '',
     githubUrl: 'https://github.com/AnthonySemprini/SessionApp',
-    description: 'Ce projet a pour but de développer une application permettant de gérer des sessions de formation et les inscriptions des stagiaires. Chaque session est limitée en nombre de places et possède des dates de début et de fin précises. L\'application offre une vue en temps réel sur le nombre de places restantes et gère un catalogue de modules de formation classés par catégorie.'
+    description: 'Ce projet vise à créer une application pour gérer les sessions de formation et les inscriptions, offrant un suivi en temps réel des places disponibles et un catalogue de modules par catégorie.'
   },
 
   {
     id: 5,
     name: 'Pierre feuille ciseaux',
     imageUrl: '/img/pierre-feuille-ciseaux.png',
+    skills: 'https://skillicons.dev/icons?i=git,html,css,js,vscode',
     projectUrl: 'https://pierre-papier-ciseaux-le-jeu.netlify.app/',
     githubUrl: 'https://github.com/AnthonySemprini/pierre-feuille-ciseaux',
     description: 'Ce projet est une implémentation web simple du jeu classique Pierre-Feuille-Ciseaux, permettant à un joueur d\'affronter l\'ordinateur.'
@@ -53,20 +58,32 @@ const projects = [
     id: 6,
     name: 'Run sausage run',
     imageUrl: '/img/run-sausage.png',
+    skills: 'https://skillicons.dev/icons?i=git,html,css,js,vscode',
     projectUrl: 'https://run-sausage.netlify.app/',
     githubUrl: 'https://github.com/AnthonySemprini/Run-sausage-RunJS',
-    description: 'Bienvenue dans "Run SAUSAGE Run!!!", un jeu dynamique et amusant où vous aidez une saucisse à éviter les obstacles pour survivre le plus longtemps possible. Ce jeu a été développé avec HTML, CSS, et JavaScript pour offrir une expérience de jeu légère et accessible directement depuis votre navigateur.'
+    description: 'Bienvenue dans \'Run SAUSAGE Run!!!\', un jeu captivant où vous guidez une saucisse à esquiver des obstacles, créé avec HTML, CSS et JavaScript pour une expérience fluide dans votre navigateur.'
   },
-  
+
   {
     id: 7,
     name: 'Tic-Tac-Toe',
     imageUrl: '/img/tic-tac-toe.png',
+    skills: 'https://skillicons.dev/icons?i=git,html,css,js,vscode',
     projectUrl: 'https://morpions-js.netlify.app/',
     githubUrl: 'https://github.com/AnthonySemprini/Tic-Tac-Toe',
-    description: 'Le jeu de Morpion se joue sur une grille de 3x3 cases. Deux joueurs s\'affrontent et marquent à tour de rôle les cases de la grille avec leur symbole respectif, "X" pour le Joueur 1 et "O" pour le Joueur 2. Le gagnant est le premier joueur à aligner trois de ses symboles horizontalement, verticalement ou diagonalement. Si la grille est complètement remplie sans qu\'aucun joueur n\'ait aligné trois symboles, la partie est déclarée nulle.'
+    description: 'Le Morpion est un jeu stratégique pour deux joueurs qui alternent pour placer \'X\' et \'O\' dans une grille de 3x3, visant à aligner trois symboles identiques pour gagner, avec match nul si la grille est pleine sans alignement.'
   },
-  
+
+  {
+    id: 8,
+    name: 'Social icons',
+    imageUrl: '/img/social.png',
+    skills: 'https://skillicons.dev/icons?i=git,html,css,vscode',
+    projectUrl: 'https://social-icons-css.netlify.app/',
+    githubUrl: 'https://github.com/AnthonySemprini/exoSocial_Icons/tree/master',
+    description: 'Le projet \'Social Icons\' offre une série d\'icônes interactives pour les réseaux sociaux, enrichissant l\'interface utilisateur par des effets visuels au survol qui révèlent le nom de chaque réseau.'
+  },
+
 ];
 
 
@@ -85,14 +102,19 @@ const Projet = () => {
         {currentProjects.map((project, index) => (
           <div key={project.id} className="custom-card-size max-w-sm rounded overflow-hidden shadow-lg transition duration-300 ease-in-out">
             <span className="text-center text-lg font-semibold block p-2"># {firstPageIndex + index + 1} #</span>
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img className='my-2' src={ project.skills } />
+              </a>
+            </p>
             <img className="w-full h-48 object-contain" src={project.imageUrl} alt={project.name} />
             <div className="px-6 py-4 min-h-[150px]">
               <div className="font-bold text-xl mb-2">{project.name}</div>
-              <p className="text-gray-700 text-base">{project.description}</p>
+              <p className="text-gray-700 text-base">{project.description}<br></br><br></br><strong> Pour en savoir plus ↓</strong></p>
               {project.projectUrl && (
-                <a href={project.projectUrl} className="text-blue-500 hover:text-blue-800 inline-block mt-4" target="_blank" rel="noopener noreferrer">Voir le projet</a>
+                <a href={project.projectUrl} className="text-blue-500 hover:text-blue-800 inline-block mt-4" target="_blank" rel="noopener noreferrer">Tester le projet</a>
               )}
-              <br></br><a href={project.githubUrl} className="text-blue-500 hover:text-blue-800 inline-block mt-2" target="_blank" rel="noopener noreferrer">Voir le repository GitHub</a>
+              <br></br><a href={project.githubUrl} className="text-blue-500 hover:text-blue-800 inline-block mt-2" target="_blank" rel="noopener noreferrer">Consulter le repository GitHub</a>
             </div>
           </div>
         ))}
