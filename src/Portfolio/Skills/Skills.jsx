4be41de918761df9babd7e95631b40d8465395ea
Anthony <semprini.anthony@gmail.com>
2024-02-26 12:14:26 +0100

    remise en forme des icones

--- src/Portfolio/Skills/Skills.jsx
+++ src/Portfolio/Skills/Skills.jsx
@@ -1,58 +1,58 @@
 import React from 'react';
 import './Skills.css';
 
-  const Skills = [
+const Skills = [
 
-    {
-      id:1,
-      name: 'Tools',
-      list :[
-        { name: 'VsCode', value: 'https://skillicons.dev/icons?i=vscode'},
-        { name: 'GitHub', value: 'https://skillicons.dev/icons?i=github' },
-        { name: 'Git' , value: 'https://skillicons.dev/icons?i=git'}
-      ]
-    },
+  {
+    id: 1,
+    name: 'Tools',
+    list: [
+      { name: 'VsCode', value: 'https://skillicons.dev/icons?i=vscode' },
+      { name: 'GitHub', value: 'https://skillicons.dev/icons?i=github' },
+      { name: 'Git', value: 'https://skillicons.dev/icons?i=git' }
+    ]
+  },
 
-    {
-      id:2,
-      name: 'Front-End',
-      list: [
-        { name: 'Html', value: 'https://skillicons.dev/icons?i=vscode'},
-        { name: 'Css', value: 'https://skillicons.dev/icons?i=css' },
-        { name: 'Javascript', value: 'https://skillicons.dev/icons?i=js' }
-      ]
-    },
- 
-    {
-      id:3,
-      name: 'Back-End',
-      list:[
-        {name:'Php', value: 'https://skillicons.dev/icons?i=php'},
-        {name:'Sql', value: 'https://skillicons.dev/icons?i=mysql'}
-      ]
-    },
+  {
+    id: 2,
+    name: 'Front-End',
+    list: [
+      { name: 'Html', value: 'https://skillicons.dev/icons?i=html' },
+      { name: 'Css', value: 'https://skillicons.dev/icons?i=css' },
+      { name: 'Javascript', value: 'https://skillicons.dev/icons?i=js' }
+    ]
+  },
+
+  {
+    id: 3,
+    name: 'Back-End',
+    list: [
+      { name: 'Php', value: 'https://skillicons.dev/icons?i=php' },
+      { name: 'Sql', value: 'https://skillicons.dev/icons?i=mysql' }
+    ]
+  },
 
 
-    {
-      id:4,
-      name: 'Frameworks',
-      list:[
-        { name: 'Symfony', value: 'https://skillicons.dev/icons?i=symfony'},
-        { name: 'Laravel', value: 'https://skillicons.dev/icons?i=laravel'},
-        { name: 'Bootstrap', value: 'https://skillicons.dev/icons?i=bootstrap'},
-        { name: 'Tailwind', value: 'https://skillicons.dev/icons?i=tailwind'}
-      ]
-    },
+  {
+    id: 4,
+    name: 'Frameworks',
+    list: [
+      { name: 'Symfony', value: 'https://skillicons.dev/icons?i=symfony' },
+      { name: 'Laravel', value: 'https://skillicons.dev/icons?i=laravel' },
+      { name: 'Bootstrap', value: 'https://skillicons.dev/icons?i=bootstrap' },
+      { name: 'Tailwind', value: 'https://skillicons.dev/icons?i=tailwind' }
+    ]
+  },
 
-    {
-      id:5,
-      name: 'Librairies',
-            list:[
-        { name:'React.js', value: 'https://skillicons.dev/icons?i=react'}
-      ]
-    },
+  {
+    id: 5,
+    name: 'Librairies',
+    list: [
+      { name: 'React.js', value: 'https://skillicons.dev/icons?i=react' }
+    ]
+  },
 
-  ];
+];
 
 
 function SkillsComponent() {
@@ -66,12 +66,18 @@ function SkillsComponent() {
         {Skills.map((skill) => (
           <div key={skill.id} className='w-full md:w-1/2 p-2'>
             <div className='flex flex-col items-center  p-4 '>
-              <h3 className='text-2xl mb-2'>{skill.name}</h3>
-              <ul className='flex flex-row list-inside gap-6'>
+              <h3 className='text-3xl font-bold mb-2'>{skill.name}</h3>
+
+              <div className='flex flex-wrap justify-center items-center gap-6'>
                 {skill.list.map((item, index) => (
-                  <li key={index} className='text-center'><img src={item.value} />{`${item.name}`} </li>
+                  <div key={index} className='flex flex-col items-center'>
+                    <img className='w-16' src={item.value} alt={item.name} />
+                    <p className='text-center text-xl font-bold'>{item.name}</p>
+                  </div>
                 ))}
-              </ul>
+              </div>
+
+
             </div>
           </div>
         ))}
