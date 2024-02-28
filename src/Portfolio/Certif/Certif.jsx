8ece7b01ef78649d122d3758f6933e11e354a495
Anthony <semprini.anthony@gmail.com>
2024-02-28 17:55:25 +0100

    modif complete de la section certif changement affichage menu accordeon par orga

--- src/Portfolio/Certif/Certif.jsx
+++ src/Portfolio/Certif/Certif.jsx
@@ -1,61 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Certif.css';
 
 function CertifComponent() {
-    const certif = [
-        { name: 'Introduction to HTML', orga: 'Sololearn, Janvier 2023', statut: 'Obtenu' },
-        { name: 'Introduction to CSS', orga: 'Sololearn, Février 2023', statut: 'Obtenu' },
-        { name: 'Responsive web Design', orga: 'FreeCodeCamp, Février 2024', statut: 'Obtenu' },
-        { name: 'A2 English for Developers', orga: 'FreeCodeCamp, Janvier 2024', statut: 'Obtenu' },
-        { name: 'Module 1 : Le RGPD et ses notions clés', orga: 'L\'atelier RGPD CNIL, Février 2024', statut: 'Obtenu' },
-        { name: 'MODULE 2 : LES PRINCIPES DE LA PROTECTION DES DONNÉES', orga: 'L\'atelier RGPD CNIL, Février 2024', statut: 'Obtenu' },
-        { name: 'Front End Development Libraries', orga: 'FreeCodeCamp', statut: 'En cours' },
-        { name: 'Fondamentaux de la cybersécurité', orga: 'Google Career Certificates', statut: 'En cours' },
-    ];
-    const forma = [
-        { name: 'Remise à niveau (Développement Web)', orga:'Elan Formation, Janvier à Avril 2023', statut: 'Obtenu' },
-        { name: 'Titre pro de niveau 5(Bac+2), développement web et web mobile', orga:'Elan Formation, Mai à Décembre 2023', statut: 'Obtenu' },
-    ];
+    const certifications = {
+     L$atelier_RGPD_Cnil: [
+        {name: 'Module 1 : Le RGPD et ses notions clés', date: 'Février 2024', statut: 'Obtenu' },
+        { name: 'Module 2 : LES PRINCIPES DE LA PROTECTION DES DONNÉES', date: 'Février 2024', statut: 'Obtenu' },
+    ],
+     Sololearn: [
+        { name: 'Introduction to HTML',date:'Janvier 2023', statut: 'Obtenu' },
+        { name: 'Introduction to CSS',date:'Mars 2023', statut: 'Obtenu' },
+
+    ],
+     FreeCodeCamp: [
+        { name: 'A2 English for Developers', date: ' Janvier 2024', statut: 'Obtenu' },
+        { name: 'Responsive web Design', date: ' Février 2024', statut: 'Obtenu' },
+        { name: 'Front End Development Libraries', date: '', statut: 'En cours' },
+    ],
+     Google_Career_Certificates: [
+        { name: 'Fondamentaux de la cybersécurité', date: '', statut: 'En cours' },
+    ],
+    };
+
+    const [openedOrga, setOpenedOrga] = useState("FreeCodeCamp");
+
+    const toggleOrga = (orga) => {
+        if (openedOrga === orga) {
+            setOpenedOrga(null); // Ferme l'accordéon si on clique sur l'organisme déjà ouvert
+        } else {
+            setOpenedOrga(orga); // Ouvre l'accordéon de l'organisme cliqué
+        }
+    };
 
     return (
         <div className="container mx-auto px-2">
-
-            <div className="forma-section">
-                <h2 className="text-2xl text-blueM montserrat font-bold text-center my-3">Mes Formations</h2>
-                <div className="grid md:grid-cols-3 gap-3">
-                    {forma.map((form, index) => (
-                        <div key={`form-${index}`} className="detail-view p-4 shadow-md rounded-lg bg-gray-200">
-                            <h3 className="montserrat text-blueM font-bold">{form.name}</h3>
-                            <p className=" poppins-regular text-blueM text-sm">{form.orga}</p>
-                            <span className={`inline-block text-blueM poppins-regular mt-2 py-1 px-3 rounded-full text-xs font-medium ${form.statut === 'Obtenu' ? 'bg-green-200 text-green-800' : 'bg-yellow-200 text-yellow-800'}`}>
-                                {form.statut}
-                            </span>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            
             <div className="certif-section">
                 <h2 className="text-2xl montserrat text-blueM font-bold text-center my-3">Mes Certifications</h2>
-                <div className="grid md:grid-cols-3 gap-3 mb-10">
-                    {certif.map((cert, index) => (
-                        <div key={index} className="detail-view p-4 shadow-md rounded-lg bg-gray-100">
-                            <h3 className="montserrat text-blueM font-bold">{cert.name}</h3>
-                            <p className="text-sm  poppins-regular text-blueM">{cert.orga}</p>
-                            <span className={`inline-block text-blueM poppins-regular mt-2 py-1 px-3 rounded-full text-xs font-medium ${cert.statut === 'Obtenu' ? 'bg-green-200 text-green-800' : 'bg-yellow-200 text-yellow-800'}`}>
-                                {cert.statut}
+                {Object.entries(certifications).map(([orga, certifs], index) => (
+                    <div key={index} className="orga-view my-4 p-4 shadow-md rounded-lg bg-gray-100 cursor-pointer" onClick={() => toggleOrga(orga)}>
+                        <h3 className="montserrat text-blueM font-bold">
+                            {orga.replace(/_/g, ' ').replace(/\$/g, "'")}
+                            <span className="float-right transform rotate-90">
+                                {openedOrga === orga ? '▼' : '▶︎'}
                             </span>
-                        </div>
-                    ))}
-                </div>
+                        </h3>
+                        {openedOrga === orga && (
+                            <div className="certif-details mt-4 text-sm poppins-regular text-blueM">
+                                {certifs.map((cert, index) => (
+                                    <div key={index} className="mt-2">
+                                        <p className='poppins-regular'>{cert.name}</p>
+                                        <p className='poppins-regular'>{cert.date}</p>
+                                        <span className={`inline-block mt-2 py-1 px-3 rounded-full text-xs font-medium ${cert.statut === 'Obtenu' ? 'bg-green-200 text-green-800' : 'bg-yellow-200 text-yellow-800'}`}>
+                                            {cert.statut}
+                                        </span>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                ))}
             </div>
-
-
         </div>
     );
+    
 }
-
-
-
 
 export default CertifComponent;
